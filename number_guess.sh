a27c7f5a8bd36b5da613d009ca7d7482d1f87fb7
nbhawnani <nisha.b@gmail.com>
2022-11-17 17:15:04 +0000

    feat: added count of tries

--- number_guess.sh
+++ number_guess.sh
@@ -17,16 +17,22 @@ else
   #generate secret number
   SECRETNUMBER=$(($RANDOM % 1000))
   echo $USERNAMEFOUND : $SECRETNUMBER
+  TRIES=0
   while [[ $GUESS -ne $SECRETNUMBER ]]
   do
     echo 'Guess the secret number between 1 and 1000:'
     read GUESS
+    TRIES=$((TRIES+1))
     if [[ ! $GUESS =~ ^[0-9]+$ ]]
     then
       #not a number
       echo 'That is not an integer, guess again:'
     fi
   done
+  #secret number is guessed
+  echo You guessed it in $TRIES tries. The secret number was $SECRETNUMBER. Nice job!
+  #insert into table
+  #NEWUSER=$($PSQL "insert into usernames(username) values('$USERNAME');")
   
 fi
 
